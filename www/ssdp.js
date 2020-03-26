c86a20d4168f92ddf9f8c8b0d2174b455cee55ef
zerofairnight <zerofairnight@gmail.com>
2020-03-26 13:03:23 +0100

    add js doc

--- www/ssdp.js
+++ www/ssdp.js
@@ -6,6 +6,10 @@ var SSDP = {
 
     /**
      * Discover the specified network services.
+     *
+     * @param {String} service
+     * @param {function(array): void} successCallback callback A function that run when the discover is successful.
+     * @param {function(any): void} errorCallback callback A function that run when the discover is successful.
      */
     getNetworkServices: function(service, successCallback, errorCallback) {
         exec(successCallback, errorCallback, 'SSDP', 'getNetworkServices', [service]);
